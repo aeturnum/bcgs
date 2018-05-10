2b9ea738bfb1174226907cb880e779457e26f8ba
Drex Drexler <aeturnum@gmail.com>
2018-05-10 19:27:00 +0000

    update run script

--- bcgs/run.sh
+++ bcgs/run.sh
@@ -1,1 +1,9 @@
-screen gunicorn --workers 3 --bind unix:bcgs.sock wsgi:app
+screen -S bb sh -c 'gunicorn wsgi:application \
+	--workers 3 \
+        --bind unix:bcgs.sock \
+        --timeout 1200 \
+        --reload \
+        --worker-class aiohttp.GunicornWebWorker \
+        --access-logfile ./logs/access.log \
+        ; exec bash
+'
